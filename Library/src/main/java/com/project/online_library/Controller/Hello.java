67f2847703658b2c6f8a39302ea79501d46f15b9
Masa Matovic <masa.matovic97@gmail.com>
2020-11-28 15:15:24 +0100

    firstt commit

---
+++ Library/src/main/java/com/project/online_library/Controller/Hello.java
@@ -0,0 +1,16 @@
+package com.project.online_library.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "Helloo!!";
+    }
+
+}
