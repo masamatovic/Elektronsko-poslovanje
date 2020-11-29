ea3683e6d6903aacee62389bfd1d9d6c40c47a3b
Masa Matovic <masa.matovic97@gmail.com>
2020-11-29 10:13:09 +0100

    spring.mvc.dispatch-options-request=true

--- Payment Concentrator/Zuul Service/src/main/java/com/project/zuul/ZuulApplication.java
+++ Payment Concentrator/Zuul Service/src/main/java/com/project/zuul/ZuulApplication.java
@@ -2,7 +2,13 @@ package com.project.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableZuulProxy
+@EnableFeignClients
 @SpringBootApplication
 public class ZuulApplication {
 
