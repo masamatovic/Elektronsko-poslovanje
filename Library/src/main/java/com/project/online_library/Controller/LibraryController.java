ad8ed8069b5ec9b1cc2e8bed3ec986c0932d88b0
AnaNikolasevic <23nikolasevic@gmail.com>
2020-12-13 21:42:03 +0100

    initial integration front fields with camunda

--- Library/src/main/java/com/project/online_library/Controller/LibraryController.java
+++ Library/src/main/java/com/project/online_library/Controller/LibraryController.java
@@ -2,16 +2,63 @@ package com.project.online_library.Controller;
 
 
 
+import java.util.List;
+
+import org.camunda.bpm.engine.FormService;
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.form.FormField;
+import org.camunda.bpm.engine.form.TaskFormData;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.project.online_library.model.FormFieldsDto;
+
+
 @RestController
 public class LibraryController {
+	@Autowired
+	IdentityService identityService;
+	
+	@Autowired
+	private RuntimeService runtimeService;
+	
+	@Autowired
+	private RepositoryService repositoryService;
+	
+	@Autowired
+	TaskService taskService;
+	
+	@Autowired
+	FormService formService;
+	
+	@GetMapping(path = "/registrationForm", produces = "application/json")
+    public @ResponseBody FormFieldsDto get() {
+		//provera da li korisnik sa id-jem pera postoji
+		//List<User> users = identityService.createUserQuery().userId("pera").list();
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("Process_01lb7it");
 
+		Task task = taskService.createTaskQuery().processInstanceId(pi.getId()).list().get(0);
+		
+		TaskFormData tfd = formService.getTaskFormData(task.getId());
+		List<FormField> properties = tfd.getFormFields();
+		for(FormField fp : properties) {
+			System.out.println(fp.getId() + fp.getType());
+		}
+		
+        return new FormFieldsDto(task.getId(), pi.getId(), properties);
+    }
+	
     @GetMapping("/libraryController")
     public ResponseEntity<String> appB(){
 
